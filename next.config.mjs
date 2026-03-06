fb6956c853d0e64c6de70b15f0eca73a824d39e1
Krishna Gelra <krishnagelra18@gmail.com>
2026-03-06 23:37:37 +0530

    Add Github Pages Development Workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+const isGitHubPages = process.env.GITHUB_ACTIONS === 'true'
+const repoName = '/Canvas-Portfolio'
+
 const nextConfig = {
+  output: 'export',
+  basePath: isGitHubPages ? repoName : '',
+  assetPrefix: isGitHubPages ? repoName : '',
   typescript: {
     ignoreBuildErrors: true,
   },
